063ab0ea1595ff12a2a43c0d446d3d980f6db212
bannzai <kingkong999yhirose@gmail.com>
2019-06-06 03:21:08 +0900

    :sparkles: Add GitHubNotificationManagerUIComponents

---
+++ GitHubNotificationManagerUIComponentsTests/GitHubNotificationManagerUIComponentsTests.swift
@@ -0,0 +1,34 @@
+//
+//  GitHubNotificationManagerUIComponentsTests.swift
+//  GitHubNotificationManagerUIComponentsTests
+//
+//  Created by Yudai.Hirose on 2019/06/06.
+//  Copyright © 2019 bannzai. All rights reserved.
+//
+
+import XCTest
+@testable import GitHubNotificationManagerUIComponents
+
+class GitHubNotificationManagerUIComponentsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
