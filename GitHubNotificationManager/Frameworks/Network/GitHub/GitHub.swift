d32e54423cba96805415231b340f58ee126ea56b
bannzai <kingkong999yhirose@gmail.com>
2019-06-05 19:35:02 +0900

    :sparkles: Add name space file

---
+++ GitHubNotificationManager/Frameworks/Network/GitHub/GitHub.swift
@@ -0,0 +1,14 @@
+//
+//  GitHub.swift
+//  GitHubNotificationManager
+//
+//  Created by Yudai.Hirose on 2019/06/05.
+//  Copyright © 2019 bannzai. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Namespace
+public enum GitHub {
+    
+}
