6e025cfaeed86f3532c383aec04e08ce7bff8e2c
bannzai <kingkong999yhirose@gmail.com>
2019-06-05 18:56:56 +0900

    :tada: Create project

---
+++ GitHubNotificationManager/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GitHubNotificationManager
+//
+//  Created by Yudai.Hirose on 2019/06/05.
+//  Copyright © 2019 bannzai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
