3fd9047ef1bd1e5756cf90d03b258db27df0d4ec
bannzai <kingkong999yhirose@gmail.com>
2019-06-06 02:37:44 +0900

    :sparkles: Fix implement of combine request

--- GitHubNotificationManager/Frameworks/GitHubNotificationManagerNetwork/Core/CoreAPIClient.swift
+++ GitHubNotificationManager/Frameworks/GitHubNotificationManagerNetwork/Core/CoreAPIClient.swift
@@ -9,15 +9,41 @@
 import Foundation
 import Combine
 
+public typealias APIResponse = (data: Data, response: HTTPURLResponse)
+public typealias RequestError = NetworkError
+public typealias APIPublisher = AnyPublisher<APIResponse, RequestError>
+
 public protocol CoreAPIClient {
-    static func request<R: APIRequest>(request: R)
+    static func request<R: APIRequest> (request: R) -> APIPublisher
 }
 
-public typealias APIResponse = (data: Data, response: HTTPURLResponse)
-public typealias RequestError = Swift.Error
-
-internal struct BaseAPIClient {
-    static func request<R: APIRequest> (request: R) -> AnyPublisher<APIResponse, RequestError> {
- fatalError()
+public enum NetworkError: Error {
+    case noResponseData
+    case networkError(Swift.Error)
+}
+internal struct BaseAPIClient: CoreAPIClient {
+    static func request<R: APIRequest> (request: R) -> APIPublisher {
+        APIPublisher { subscriber in
+            let session = URLSession(configuration: URLSessionConfiguration.default)
+            let task = session.dataTask(with: request.url) { (data, response, error) in
+                guard let data = data, let httpReponse = response as? HTTPURLResponse else {
+                    return subscriber.receive(completion: .failure(.noResponseData))
+                }
+                if let error = error {
+                    return subscriber.receive(completion: .failure(.networkError(error)))
+                }
+                
+                switch subscriber.receive((data, httpReponse)) {
+                case .max(let count):
+                    if count == 0 { // FIXME: Not found reference for max.
+                        subscriber.receive(completion: .finished)
+                        session.invalidateAndCancel()
+                    }
+                case .unlimited:
+                    break
+                }
+            }
+            task.resume()
+        }
     }
 }
