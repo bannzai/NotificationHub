b09a5111b070e2e82b72abf85318c2d920c61e12
bannzai <kingkong999yhirose@gmail.com>
2019-09-28 11:42:01 +0900

    :recycle: Integrate GitHub API Client

--- GitHubNotificationManager/Frameworks/GitHubNotificationManagerNetwork/GitHub/GitHubAPI.swift
+++ GitHubNotificationManager/Frameworks/GitHubNotificationManagerNetwork/GitHub/GitHubAPI.swift
@@ -9,19 +9,37 @@
 import Foundation
 import Combine
 
+// TODO: Use NetworkError
+public typealias RequestError = Swift.Error
+
+public enum NetworkError: Error {
+    case noResponseData
+    case networkError(Swift.Error)
+    case makeURLRequestFaield(Swift.Error)
+}
+
+
 public struct GitHubAPI {
-    public static func request<R: APIRequest> (request: R) -> AnyPublisher<R.Response, Error> where R.Response: Decodable {
-        BaseAPIClient.request(request: request)
-            .map { $0.data }
-            .decode(type: R.Response.self, decoder: JSONDecoder())
-            .handleEvents(receiveCompletion: { (completion) in
-                switch completion {
-                case .finished:
-                    print("[INFO] Finished")
-                case .failure(let error):
-                    print("[ERRROR] error: \(error)")
-                }
-            })
-            .eraseToAnyPublisher()
+    internal struct DecodedRequestPublisher<Request: APIRequest>: Publisher where Request.Response: Decodable {
+        typealias Output = Request.Response
+        typealias Failure = RequestError
+        
+        let request: Request
+        
+        func receive<S>(subscriber: S) where S : Subscriber, DecodedRequestPublisher.Failure == S.Failure, DecodedRequestPublisher.Output == S.Input {
+            do {
+                URLSession.shared.dataTaskPublisher(for: try request.urlRequest())
+                    .map { $0.data }
+                    .decode(type: Output.self, decoder: JSONDecoder())
+                    .subscribe(subscriber)
+            } catch {
+                Fail<Output, Swift.Error>(error: error)
+                    .subscribe(subscriber)
+            }
+        }
+    }
+    
+    public static func request<R: APIRequest> (request: R) -> AnyPublisher<R.Response, RequestError> where R.Response: Decodable {
+        DecodedRequestPublisher(request: request).eraseToAnyPublisher()
     }
 }
