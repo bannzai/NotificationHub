f1cb77b58bae6d716a8e28ac4f2aa15dbd171b4c
bannzai <kingkong999yhirose@gmail.com>
2019-09-29 15:17:31 +0900

    :sparkles: Fix present safari view
    #
    # ==== Emojis ====
    # 🐛  :bug: バグ修正
    # 🤘  :metal: 機能改善
    # 🐚  :shell: コマンドを実行した結果とか
    # ✨  :sparkles: 部分的な機能追加
    # 🎉  :tada: 盛大に祝うべき大きな機能追加
    # 💉  :syringe: libraryの追加
    # 📝  :memo: ドキュメント・READMEの整備
    # ♻️   :recycle: リファクタリング
    # 🚿  :shower: 不要な機能・使われなくなった機能の削除
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # 🆙  :up: 依存パッケージなどのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🎨  :art: デザイン修正
    # 🚧  :construction: WIP 一時的なコミット
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # reference from http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/
    # for https://github.com/bannzai/commit_template

--- GitHubNotificationManager/Screen/WebView/SafariView.swift
+++ GitHubNotificationManager/Screen/WebView/SafariView.swift
@@ -9,6 +9,64 @@
 import SwiftUI
 import SafariServices
 
+class SafariViewControllerWrapperViewController: UIViewController {
+    var url: URL {
+        didSet {
+            removeChildViewControllerIfNeeded()
+            createContentViewcontroller()
+        }
+    }
+    
+    init(url: URL) {
+        self.url = url
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private var safariViewController: SafariViewController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        removeChildViewControllerIfNeeded()
+        createContentViewcontroller()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        removeChildViewControllerIfNeeded()
+    }
+    
+    func removeChildViewControllerIfNeeded() {
+        if let safariViewController = safariViewController {
+            safariViewController.view.constraints.forEach { $0.isActive = false }
+            safariViewController.willMove(toParent: self)
+            safariViewController.view.removeFromSuperview()
+            safariViewController.removeFromParent()
+        }
+        safariViewController = nil
+    }
+    
+    func createContentViewcontroller() {
+        let safariViewController = SafariViewController(url: url)
+        safariViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        addChild(safariViewController)
+        view.addSubview(safariViewController.view)
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: safariViewController.view.topAnchor),
+            view.bottomAnchor.constraint(equalTo: safariViewController.view.bottomAnchor),
+            view.leftAnchor.constraint(equalTo: safariViewController.view.leftAnchor),
+            view.rightAnchor.constraint(equalTo: safariViewController.view.rightAnchor),
+        ])
+        safariViewController.didMove(toParent: self)
+        self.safariViewController = safariViewController
+    }
+}
+
 class SafariViewController: SFSafariViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,24 +84,21 @@ class SafariViewController: SFSafariViewController {
 }
 
 extension SafariViewController: SFSafariViewControllerDelegate {
-    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
-        controller.navigationController?.popViewController(animated: true)
-    }
+    
 }
 
 struct SafariView : UIViewControllerRepresentable {
-    typealias UIViewControllerType = SFSafariViewController
-    
+    typealias UIViewControllerType = SafariViewControllerWrapperViewController
+
     let url: URL
     func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SafariView.UIViewControllerType {
-        let viewController = SafariViewController(url: url)
+        let viewController = SafariView.UIViewControllerType(url: url)
         return viewController
     }
     
-    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
-        uiViewController.navigationController?.setNavigationBarHidden(true, animated: false)
+    func updateUIViewController(_ uiViewController: SafariView.UIViewControllerType, context: UIViewControllerRepresentableContext<SafariView>) {
+        uiViewController.url = url
     }
-    
 }
 
 #if DEBUG
