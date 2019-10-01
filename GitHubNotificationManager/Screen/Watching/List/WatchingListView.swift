9e99d5e20f957b70f39be5c5b89c900c1d001cfb
bannzai <kingkong999yhirose@gmail.com>
2019-10-01 18:34:09 +0900

    :sparkles: Handle fetch watchings to RootView
    #
    # ==== Emojis ====
    # 🐛  :bug: バグ修正
    # 🤘  :metal: 機能改善
    # 🐚  :shell: コマンドを実行した結果とか
    # ✨  :sparkles: 部分的な機能追加
    # 🎉  :tada: 盛大に祝うべき大きな機能追加
    # 💉  :syringe: libraryの追加
    # 📝  :memo: ドキュメント・READMEの整備
    # ♻️   :recycle: リファクタリング
    # 🚿  :shower: 不要な機能・使われなくなった機能の削除
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # 🆙  :up: 依存パッケージなどのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🎨  :art: デザイン修正
    # 🚧  :construction: WIP 一時的なコミット
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # reference from http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/
    # for https://github.com/bannzai/commit_template

--- GitHubNotificationManager/Screen/Watching/List/WatchingListView.swift
+++ GitHubNotificationManager/Screen/Watching/List/WatchingListView.swift
@@ -9,42 +9,28 @@
 import SwiftUI
 
 struct WatchingListView: View {
-    @ObservedObject private var viewModel: WatchingListViewModel
-
     // FIXME: Keep data when presented this view
-    var fetched: ([WatchingModel]) -> ()
-
-    init(
-        watchings defaultWatchings: [WatchingModel],
-        fetched: @escaping ([WatchingModel]) -> ()
-    ) {
-        self.fetched = fetched
-        self.viewModel = WatchingListViewModel(watchings: defaultWatchings)
-    }
+    @Binding var watchings: [WatchingModel]
 
     func watching(of index: Int) -> Binding<WatchingModel> {
         Binding(get: {
-            self.viewModel.watchings[index]
+            self.watchings[index]
         }) { (watching) in
-            self.viewModel.watchings[index] = watching
+            self.watchings[index] = watching
         }
     }
     
     var body: some View {
-        List(viewModel.watchings.indices, id: \.self) { (index) in
+        List(watchings.indices, id: \.self) { (index) in
             Cell(watching: self.watching(of: index))
-        }.onReceive(viewModel.$watchings, perform: { (watchings) in
-            self.fetched(watchings)
-        }).onAppear {
-            self.viewModel.fetch()
         }
     }
 }
 
 #if DEBUG
-struct WatchingListView_Previews: PreviewProvider {
-    static var previews: some View {
-        WatchingListView(watchings: [], fetched: { _ in })
-    }
-}
+//struct WatchingListView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        WatchingListView(watchings: [], fetched: { _ in })
+//    }
+//}
 #endif
