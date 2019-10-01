26060e5004558b2b14dc688b17579adb02de5dec
bannzai <kingkong999yhirose@gmail.com>
2019-10-01 12:17:18 +0900

    :sparkles: Add wrapper for notification list view

---
+++ GitHubNotificationManager/Screen/Notification/List/NotificationListPageView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationListPageView.swift
+//  GitHubNotificationManager
+//
+//  Created by Yudai.Hirose on 2019/10/01.
+//  Copyright © 2019 bannzai. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationListPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificationListPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationListPageView()
+    }
+}
