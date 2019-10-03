cd1e7dbacedcad6283caf58aca92d2babf13aed5
bannzai <kingkong999yhirose@gmail.com>
2019-10-04 00:29:27 +0900

    :sparkles: Add OAuthViewController

---
+++ GitHubNotificationManager/Screen/OAuth/OAuthViewController.swift
@@ -0,0 +1,14 @@
+//
+//  OAuthViewController.swift
+//  GitHubNotificationManager
+//
+//  Created by Yudai.Hirose on 2019/10/04.
+//  Copyright © 2019 bannzai. All rights reserved.
+//
+
+import UIKit
+import OAuthSwift
+
+public class OAuthViewController: UIViewController {
+    
+}
