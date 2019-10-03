f49226c159659a8ed51f564c7c328c52fed4000c
bannzai <kingkong999yhirose@gmail.com>
2019-10-04 00:02:36 +0900

    :memo: Fix replace command for info.plist

--- scripts/development/info.plist.sh
+++ scripts/development/info.plist.sh
@@ -5,4 +5,10 @@ set -o pipefail
 PWD=`dirname $0`
 INFO_PLIST_PATH=$PWD/../../GitHubNotificationManager/
 
-cp -i $INFO_PLIST_PATH/Info.plist.sample $INFO_PLIST_PATH/Info.plist
+set +e
+cp -i $INFO_PLIST_PATH/Info.plist.sample $INFO_PLIST_PATH/Info.plist 
+set -e
+
+printf "Start to replace from GITHUB_NOTIFICATION_MANAGER_URL_SCHEMA to %s for %s\n" $GITHUB_NOTIFICATION_MANAGER_URL_SCHEMA $INFO_PLIST_PATH/Info.plist
+
+sed -i '' -e "s/GITHUB_NOTIFICATION_MANAGER_URL_SCHEMA/$GITHUB_NOTIFICATION_MANAGER_URL_SCHEMA/" $INFO_PLIST_PATH/Info.plist
