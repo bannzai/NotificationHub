deafb800a2f04dedc831051c2ce8711577e86ab0
bannzai <kingkong999yhirose@gmail.com>
2019-10-09 09:06:20 +0900

    :sparkles: Add singleton for coredata accessor

--- GitHubNotificationManager/SceneDelegate.swift
+++ GitHubNotificationManager/SceneDelegate.swift
@@ -64,7 +64,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        CoreDataAccessor.shared.saveContext()
     }
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
