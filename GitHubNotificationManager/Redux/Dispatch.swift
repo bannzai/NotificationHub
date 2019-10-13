2e87daee8f85972791db486de9bd5f781e40c7fd
bannzai <kingkong999yhirose@gmail.com>
2019-10-13 12:48:37 +0900

    :sparkles: Add redux componenets

---
+++ GitHubNotificationManager/Redux/Dispatch.swift
@@ -0,0 +1,11 @@
+//
+//  Dispatch.swift
+//  GitHubNotificationManager
+//
+//  Created by Yudai Hirose on 2019/10/13.
+//  Copyright © 2019 bannzai. All rights reserved.
+//
+
+import Foundation
+
+public typealias DispatchFunction = (Action) -> Void
