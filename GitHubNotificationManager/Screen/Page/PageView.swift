f5dd10882b0bc81d0649ae08d02bea22c00b9710
bannzai <kingkong999yhirose@gmail.com>
2019-10-14 11:53:01 +0900

    Revert ":recycle: Remove page view"
    This reverts commit 7f96d3584a3e241b952f1f8a5a562bd0eeb2f739.

---
+++ GitHubNotificationManager/Screen/Page/PageView.swift
@@ -0,0 +1,56 @@
+//
+//  PageView.swift
+//  GitHubNotificationManager
+//
+//  Created by Yudai.Hirose on 2019/09/30.
+//  Copyright © 2019 bannzai. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+struct NotificationListPageView: View {
+    @EnvironmentObject var store: Store<AppState>
+    @Binding var currentPage: Int
+
+    var pages: [NotificationListView] {
+        return store
+            .state
+            .notificationPageState
+            .notificationsStatuses
+            .filter { $0.isVisible }
+            .map { _ in
+                NotificationListView()
+        }
+    }
+    
+    var body: some View {
+        PageView(views: pages, currentPage: $currentPage)
+    }
+}
+
+struct PageView<Page: View>: View {
+    var viewControllers: [UIHostingController<Page>]
+    var currentPage: Binding<Int>
+
+    init(views: [Page], currentPage: Binding<Int>) {
+        self.viewControllers = views.map { UIHostingController(rootView: $0) }
+        self.currentPage = currentPage
+    }
+
+    var body: some View {
+        PageViewController(
+            viewControllers: viewControllers,
+            currentPage: currentPage
+        )
+    }
+}
+
+#if DEBUG
+struct PageView_Preview: PreviewProvider {
+    @State static var currentPage: Int = 0
+    static var previews: some View {
+        PageView(views: [EmptyView()], currentPage: $currentPage)
+    }
+}
+#endif
